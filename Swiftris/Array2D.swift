f305665f545ab49696a9d0d633abb1372980384e
Anthony John <anthony.john3113@gmail.com>
2014-12-04 00:09:09 +0800

    Initial commit from local git

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,35 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Anthony John Garcia on 3/12/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+import Foundation
+
+class Array2D<T>
+{
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int)
+    {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T?
+    {
+        get
+        {
+            return array[(row * columns) + column]
+        }
+        set(newValue)
+        {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
